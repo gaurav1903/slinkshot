1217afc8925da9cd61b1e0532d51db6691a18025
gaurav arora <gaurav1903arora@gmail.com>
2021-03-03 20:37:23 +0530

    just a few additions

--- lib/screens/skin_market_screen.dart
+++ lib/screens/skin_market_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SkinMarketScreen extends StatelessWidget {
@@ -8,22 +9,48 @@ class SkinMarketScreen extends StatelessWidget {
           title: Text('skin market page'),
         ),
         body: Container(
-          padding: EdgeInsets.all(20),
-          color: Colors.black,
+          padding: EdgeInsets.all(10),
+          color: Color(0xFF1B1818),
           child: GridView.builder(
+              itemCount: 15,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, crossAxisSpacing: 20, mainAxisSpacing: 20),
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                  childAspectRatio: 1),
               itemBuilder: (ctx, index) {
                 return GridTile(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        gradient: LinearGradient(
-                            colors: [Colors.purple, Colors.white])),
-                    padding: EdgeInsets.all(10),
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage('images/kratos2.jpg'),
-                    ),
+                  child: Column(
+                    children: [
+                      Text('Title',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        height: 110,
+                        width: 110,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                                colors: [Colors.purple, Colors.white])),
+                        padding: EdgeInsets.all(10),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('images/kratos2.jpg'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('\$600',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                    ],
                   ),
                 );
               }),
