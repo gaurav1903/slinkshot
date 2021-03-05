0c3f7982146309f2efe03661ead342e81d781686
gaurav arora <gaurav1903arora@gmail.com>
2021-03-05 16:07:52 +0530

    sample data colour changed

--- lib/dataprovider/skindata.dart
+++ lib/dataprovider/skindata.dart
@@ -4,22 +4,16 @@ import 'package:slinkshot/model/skins.dart';
 List<Skin> skins1 = [
   Skin(
       imagepath: 'images/Goku1.jpg',
-      title: 'Goku',
+      title: 'Dragon Ball Z',
       color1: Color(0xFFCB7746),
       amount: 100,
       color2: Color(0xFF2A2870)),
   Skin(
       imagepath: 'images/cod2.png',
-      title: 'CallOfDuty',
+      title: 'Call Of Duty',
       color1: Color(0xFFDDCF92),
       amount: 100,
       color2: Color(0xFF302116)),
-  Skin(
-      imagepath: 'images/kratos2.jpg',
-      title: 'GodOfWar',
-      color1: Color(0xFF9F9A98),
-      amount: 100,
-      color2: Color(0xFF464E48)),
   Skin(
       imagepath: 'images/witcher.jpg',
       title: 'Witcher',
@@ -28,10 +22,16 @@ List<Skin> skins1 = [
       color2: Color(0xFFC2232D)),
   Skin(
       imagepath: 'images/pop2.jpg',
-      title: 'PrinceOfPersia',
+      title: 'Prince Of Persia',
       color1: Color(0xFFDDCF92),
       amount: 100,
       color2: Color(0xFF302116)),
+  Skin(
+      imagepath: 'images/kratos2.jpg',
+      title: 'God Of War',
+      color1: Color(0xFF9F9A98),
+      amount: 100,
+      color2: Colors.red),
 ];
 List<Skin> skins2 = [
   Skin(
@@ -42,7 +42,7 @@ List<Skin> skins2 = [
       color2: Color(0xFF663021)),
   Skin(
       imagepath: 'images/doom2.jpg',
-      title: 'DoomEternal',
+      title: 'Doom Eternal',
       color1: Color(0xFF0DA3D),
       amount: 100,
       color2: Color(0xFFF7230D)),
@@ -54,19 +54,19 @@ List<Skin> skins2 = [
       color2: Color(0xFF0E1013)),
   Skin(
       imagepath: 'images/Goku1.jpg',
-      title: 'Goku',
+      title: 'Dragon Ball Z',
       color1: Color(0xFFCB7746),
       amount: 100,
       color2: Color(0xFF2A2870)),
   Skin(
       imagepath: 'images/cod2.png',
-      title: 'CallOfDuty',
+      title: 'Call Of Duty',
       color1: Color(0xFFDDCF92),
       amount: 100,
       color2: Color(0xFF302116)),
   Skin(
       imagepath: 'images/kratos2.jpg',
-      title: 'GodOfWar',
+      title: 'God Of War',
       color1: Color(0xFF9F9A98),
       amount: 100,
       color2: Color(0xFF464E48)),
@@ -92,7 +92,7 @@ List<Skin> skins3 = [
       color2: Colors.black),
   Skin(
       imagepath: 'images/cyberpunk1.jpg',
-      title: 'CyberPunk2077',
+      title: 'CyberPunk 2077',
       color1: Color(0xFF8C4B08),
       amount: 100,
       color2: Color(0xFFCFA111)),
